003708001b1e80ecf0281e51c72acbecef57670f
abdulrahman772 <75700837+abdulrahman772@users.noreply.github.com>
2024-06-21 18:58:49 +0300

    Add files via upload

---
+++ patient/include/fun/function.php
@@ -0,0 +1,54 @@
+<?php
+function getTitle()
+{
+ global $page_title ;
+   if (isset($page_title )) {
+     echo $page_title ;
+   }
+   else {
+     echo "defoult";
+   }
+ }
+
+
+ function checkitem($select, $from, $value){
+ global $con;
+
+ $stmt = $con->prepare("SELECT $select FROM $from WHERE $select =? ");
+ $stmt->execute(array($value));
+   $count = $stmt->rowCount();
+ return $count ;
+
+
+
+ }
+
+ function redirecthome($theMsg, $url= null, $seconds = 1 ){
+
+   if ($url === null ) {
+     $url = 'dashboard.php';
+   }
+   elseif ($url === 'nnn') {
+       $url = 'dashboard.php';
+   }
+   else {
+     if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] !== '') {
+       $url = $_SERVER['HTTP_REFERER'];
+     }
+     else {
+     $url = 'dashboard.php';
+     }
+
+   }
+ echo $theMsg;
+ exit();
+
+
+
+
+ }
+
+
+
+
+  ?>
