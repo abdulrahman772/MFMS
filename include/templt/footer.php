77cc459750463602195a926e39b1321342fa7b5c
abdulrahman772 <75700837+abdulrahman772@users.noreply.github.com>
2024-06-21 19:09:28 +0300

    Add files via upload

---
+++ include/templt/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+</div>
+<script src="layout/js/jquery-1.10.2.min.js"></script>
+<script src="layout/js/bootstrap.min.js"></script>
+<script src="layout/js/backand.js"></script>
+
+</body>
+</html>
