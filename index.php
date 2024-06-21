8d992619beac74be02259d8627d09b84dda2059d
abdulrahman772 <75700837+abdulrahman772@users.noreply.github.com>
2024-06-21 18:25:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>MFMS</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="layout/css/bootstrap.min.css">
+    <link rel="stylesheet" href="layout/css/font-awesome.min.css">
+    <link rel="stylesheet" href="layout/css/owl.carousel.css">
+    <link rel="stylesheet" href="layout/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="layout/css/tooplate-style.css">
+<style>
+@font-face {
+    font-family: myfont;
+    src: url("layout/fonts/fontmfms.ttf");
+}
+</style>
+</head>
+<body id="top" data-spy="scroll" style="font-family:myfont;">
+<section id="home" class="slider" data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div dir="rtl" class="container">
+            <div class="container rigthtoop">
+
+                <a href="aboutpro.html" ><img src="layout/image/36.ico" alt="عن المبرمج"></a>
+            </div>
+        </div>
+        <div dir="rtl" class="container-fluid">
+            <div class=" bodyindex">
+                <a href="patient/login.php?do=mmm">
+                <button class="allwork  sick " onclick="loginall_open()">
+                    <img src="layout/image/26.ico" alt="iconsick" class="allicon imgsick">
+                    <p>الملف الطبي</p>
+                </button>
+                </a>
+                <br>
+                <a href="doctor/login.php?do=mmm">
+                <button class="allwork doctor" onclick="loginall_open()">
+                    <img src="layout/image/27.ico" alt="icondoctor" class="allicon imgdoctor">
+                    <p>الطبيب</p>
+                </button>
+                  </a>
+                <br>
+                <a href="admin/login.php?do=mmm">
+                <button class="allwork admin" onclick="loginall_open()">
+                    <img src="layout/image/28.ico" alt="icondoctor" class="allicon imgadmin">
+                    <p>المسئول</p>
+                </button>
+                  </a>
+            </div>
+            <style>
+              .flagsys{
+                position: absolute;
+                margin: 0 auto;
+                text-align: center;
+                z-index: 2000;
+                right: 40%;
+                top: 3%;
+              }
+              .flagsys img{
+              }
+            </style>
+            <div class="flagsys">
+              <img src="layout/image/qq.png" alt="flagsys" width="70%" height="35%">
+            </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="owl-carousel owl-theme">
+                <div class="item item-first" style="height:800px;">
+                    <div class="caption">
+                    </div>
+                </div>
+
+                <div class="item item-second" style="height:800px;">
+                    <div class="caption">
+                    </div>
+                </div>
+
+                <div class="item item-third" style="height:800px;">
+                    <div class="caption">
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+</section>
+<div class="">
+    <div class="" dir="ltr" id="shawo">
+      <nav class='navbar navbar-default'>
+        <div class='container-fluid'>
+              <div class='navbar-header' id='head'>
+              <button type='button' id='iii' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>
+              <span class='icon-bar'></span>
+              <span class='icon-bar'></span>
+              <span class='icon-bar'></span>
+              </button>
+              <a class='navbar-brand' href='#myPage'>MFMS</a>
+              </div>
+              <div class='collapse navbar-collapse' id='myNavbar'>
+              <ul class='nav navbar-nav navbar-right'>
+              <li><a href='#myPage' class='home'>الصفحه الرئيسيه</a></li>
+              <li><a href='#band'>الملف الطبي</a></li>
+              <li><a href='#tour'>الطبيب</a></li>
+              <li><a href='#contact'>المسئول</a></li>
+              </ul>
+              </div>
+              </div>
+              </nav>
+              <div id="myPage" class='container-fluid bodybage text-center'>
+                  <h1 class='text-center address'>MFMS<SPAN>!!</SPAN></h1>
+                  <small class='text-center title'><span>M</span>EDICAL <span>F</span>ILE <span>M</span>ANAGEMENT <span>S</span>YSTEM</small>
+                  <p style="height:500px;">وهو <span>نظام ادارة ملف طبي: </span>يقوم بحفظ جميع بيانات المريض مثل: التقارير,والعلاجات ,والفحوصات ,والاشعة ويستطيع المريض الوصول اليها بسهوله
+                   وأيضا يسهل على الطبيب متابعة المريض , لاننا نعاني من مشكله كبيرة مثل: سفر الطبيب ,او تغيير الطبيب ,او عدم قدرة المريض على دفع تكاليف الطبيب الاول فيقوم بتغييرة فان النظام يسهل على الطبيب الاخر تشخيص المريض ومعرفة جميع بياناته... .
+                   ويسهل على المريض اشياء كثيره مثلاً : لايضطر المريض لإعادة الاشعه الضائعه او المنسيه في المنزل وذالك لانها موجوده في النظام ,عدم حمل الاوراق معك الى اي مكان ,وضياع برشور العلاج ,عدم انتظار المريض مده من الوقت لحصوله على الفحص او الاشعه... .
+                  ويسهل على المستخدم ايضا مثل :ترتيب مواعيد المرضى ,عدم انتظار المريض في المستشفى فتخف الزحام
+                  </p>
+              </div>
+            <div id="band" class='container-fluid bodybage text-center' >
+                <h1 class='text-center address'>MFMS<SPAN>!!</SPAN></h1>
+                <small class='text-center title'><span>M</span>EDICAL <span>F</span>ILE <span>M</span>ANAGEMENT <span>S</span>YSTEM</small>
+                <p style="height:500px;"> يتميز <span> الملف الطبي </span>بحماية بيانات المريض من  فقدانها او ضياعها وايضا  السرية التامه لبيانات المريض بحيث لا يستطيع احد الاطلاع
+                    على بياناته الا بأمر منه وايضاً يستطيع الاطلع على بياناته باأي وقت وأي مكان في حال كان مسجل في<span> نظام ادارة الملف الطبي. </span>
+                </p>
+            </div>
+            <div id="tour" class='container-fluid bodybage text-center'>
+                <h1 class='text-center address'>MFMS<SPAN>!!</SPAN></h1>
+                <small class='text-center title '><span>M</span>EDICAL <span>F</span>ILE <span>M</span>ANAGEMENT <span>S</span>YSTEM</small>
+                <p style="height:500px;"> من المميزات <span> للطبيب  </span>انه سهل عليه معرفة بيانات المريض لمتابعة حالةُ من حيث الاطلع على الفحوصات والتقارير
+                    السابقة للمريض في حال قد عرُض على طبيب اخرى في نفس المستشفى او تم عرض الفحوصات والتقارير من قبل المريض نفسه.
+                </p>
+            </div>
+            <div id="contact" class='container-fluid bodybage text-center'>
+                <h1 class='text-center address'>MFMS<SPAN>!!</SPAN></h1>
+                <small class='text-center title'><span>M</span>EDICAL <span>F</span>ILE <span>M</span>ANAGEMENT <span>S</span>YSTEM</small>
+                <p style="height:500px;"> من المميزات <span> المسئول  </span>
+                      يمكن للمسئول اضافة وتعديل وحذف وبحث والتحكم بالمرضى والاطباء حيث ويمكنه  اضافة اشعة لمريض
+                </p>
+            </div>
+
+    </div>
+
+</div>
+<script src="layout/js/jquery.js"></script>
+<script src="layout/js/jquery.min.js"></script>
+<script src="layout/js/bootstrap.min.js"></script>
+<script src="layout/js/owl.carousel.min.js"></script>
+<script src="layout/js/custom.js"></script>
+</body>
+</html>
